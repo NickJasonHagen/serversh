49b6d91f589dd2ca45f00e71d260eda2fc4d2b09
Nick Jason Hagen <nickjasonhagen@gmail.com>
2026-03-10 19:02:15 +0100

    golang

--- setup.sh
+++ setup.sh
@@ -20,3 +20,8 @@ sudo apt install xrdp -y
 sudo systemctl enable --now xrdp
 sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl
 curl https://pyenv.run | bash
+
+# golang
+sudo add-apt-repository ppa:longsleep/golang-backports
+sudo apt update
+sudo apt install golang-go
