533eaca6eaccb4235dca55641ad7c9da7093fa51
Nick Jason Hagen <nickjasonhagen@gmail.com>
2026-03-10 18:56:58 +0100

    xrdp

--- setup.sh
+++ setup.sh
@@ -1,13 +1,17 @@
+
+sudo apt update
 sudo apt install samba
 sudo apt install docker.io
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
   sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-  sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list   
+  sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
-  sudo apt-get update   
-  sudo apt-get install -y nvidia-container-toolkit   
-  sudo nvidia-ctk runtime configure --runtime=docker   
-  sudo systemctl restart docker   
-  #sudo docker run --rm --gpus all nvidia/cuda:12.4.0-base-ubuntu22.04 nvidia-smi   
+  sudo apt-get update
+  sudo apt-get install -y nvidia-container-toolkit
+  sudo nvidia-ctk runtime configure --runtime=docker
+  sudo systemctl restart docker
+  #sudo docker run --rm --gpus all nvidia/cuda:12.4.0-base-ubuntu22.04 nvidia-smi
 sudo docker run -d -p 7777:8080 --gpus=all -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama
+sudo apt install xrdp -y
+sudo systemctl enable --now xrdp
