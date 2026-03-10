bf3f281910cf8efbdfb8a0561fe92fd74da136a4
Nick Jason Hagen <nickjasonhagen@gmail.com>
2026-03-10 18:55:17 +0100

    samba

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+sudo apt install samba
 sudo apt install docker.io
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
